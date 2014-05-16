43e3683bb447b916b27779543de3583c73949fac
YuvrajPatel003 <user1@Administrators-iMac-85.local>
2014-05-16 14:01:45 -0400

    c

---
+++ varun/varun/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  varun
+//
+//  Created by User1 on 2014-05-15.
+//  Copyright (c) 2014 Objective. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
